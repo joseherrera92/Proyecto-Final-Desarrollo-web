a95352627cc5454e583f75866c652f8a13edbcb6
James Loaiza <110996018+srjamesgg@users.noreply.github.com>
2022-10-25 17:55:21 -0500

    Squashed commit of the following:
    commit 24087be6af998a28f2ccc6ef263ce087d4b2cf22
    Author: James Loaiza <110996018+srjamesgg@users.noreply.github.com>
    Date:   Tue Oct 25 17:48:15 2022 -0500
    
        cambios en reglamentos
    
        se hicieron cambios en reglamento.jv y se agrego reglamento.css

--- proyecto/src/componentes/Reglamentos.js
+++ proyecto/src/componentes/Reglamentos.js
@@ -1,23 +1,45 @@
 import react from 'react';
+import styles from "./css/Reglamentos.css"
 
 const Reglamentos = () => {
     return(
         <section className='reglamentos'>
-            <article className='reglamento liga'>
+            <article className='reglamento-liga'>
                 <></>
-                <h3> Reglamento liga</h3>
+                <h2> Reglamento liga</h2>
+
             </article>
 
-            <article className='reglamento festival'>
-                <h3> Reglamento festival</h3>
+            <article className='reglamento-festival'>
+                <h2> Reglamento festival</h2>
+                <h3> Categoria SUB 7 y 8</h3>
+                <p>
+                 <p> TIEMPO JUGADO: 3 tiempos cada uno de quince minutos</p>
+                 <p>NUMERO DE JUGADORES: 5</p>
+                 <p> BALON: #3</p>
+                 <p>  FIGURA DE LUGAR: no aplica</p>
+                </p>
+
+
+                <h3> Categoria Sub 9 y 10 </h3>
+                <p>
+                   <p> TIEMPO JUGADO: 3 tiempos de 20 minutos</p>
+                   <p> NUMERO DE JUGADORES: 7</p>
+                   <p> BALON: #3</p>
+                   <p> FUERA DE LUGAR: no aplica</p>
+
+
+                </p>
+
+
             </article>
             
-            <article className='reglamento salado'>
-                 <h3> Reglamento salado</h3>
+            <article className='reglamento-salado'>
+                 <h2> Reglamento salado</h2>
             </article>
                 
-            <article className='regalemnto san ignacio'>
-                 <h3> Reglamento san ignacio</h3>
+            <article className='regalemnto-san-ignacio'>
+                 <h2> Reglamento san ignacio</h2>
             </article>
         </section>
     );
